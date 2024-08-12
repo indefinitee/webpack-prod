097e7d54bf00474c77404a7659a6c7063318c1c3
indefinitee <danny.galaev@gmail.com>
2024-08-12 20:11:54 +0400

    возможность смены темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Suspense, useContext, useState } from 'react';
 import { Counter } from './components/Counter';
-import './index.scss';
+import './styles/index.scss';
+import { Link, Route, Routes } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Сменить тему</button>
+            <Link to={"/about"}>Главная</Link>
+            <Link to={"/"}>О сайте</Link>
+            <Suspense fallback={<div>Загрузка...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
             <Counter />
         </div>
     );
