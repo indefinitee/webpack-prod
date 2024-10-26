26d2e8cf1f19f55514fc6466b4787860d8b93b56
indefinitee <danny.galaev@gmail.com>
2024-10-26 18:44:31 +0400

    fix linter errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,8 +38,14 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': 2,
-        'max-len': ['error', { ignoreComments: true }],
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-testid', 'to'],
+            },
+        ],
+        'max-len': ['error', { ignoreComments: true, code: 120 }],
     },
     globals: {
         __IS_DEV__: true,
