f709110b6c39b3860c2e136c67a908aa0157a19b
indefinitee <danny.galaev@gmail.com>
2024-11-04 17:08:12 +0400

    ui test -> chrome.docker

--- config/storybook/main.ts
+++ config/storybook/main.ts
@@ -5,7 +5,6 @@ const config: StorybookConfig = {
         '../../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
     ],
     addons: [
-        '@storybook/addon-webpack5-compiler-swc',
         '@storybook/addon-onboarding',
         '@storybook/addon-links',
         '@storybook/addon-essentials',
