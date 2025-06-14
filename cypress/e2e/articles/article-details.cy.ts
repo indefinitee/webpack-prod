20ca2b9284860b9bf5299aec2acfc1cf3c7f0980
indefinitee <danny.galaev@gmail.com>
2025-06-14 22:40:39 +0300

    add fixtures

--- cypress/e2e/articles/article-details.cy.ts
+++ cypress/e2e/articles/article-details.cy.ts
@@ -7,22 +7,35 @@ describe('Пользователь заходит на страницу стат
             cy.visit(`articles/${article.id}`);
         });
     });
+
     afterEach(() => {
         cy.removeArticle(currentArticleId);
     });
+
     it('И видит содержимое статьи', () => {
         cy.getByTestId('ArticleDetails.Info').should('exist');
     });
+
     it('И видит список рекоммендаций', () => {
         cy.getByTestId('ArticleRecommendationsList').should('exist');
     });
+
     it('И оставляет комментарий', () => {
         cy.getByTestId('ArticleDetails.Info');
         cy.getByTestId('AddCommentForm').scrollIntoView();
         cy.addComment('text');
         cy.getByTestId('CommentCard.Content').should('have.length', 1);
     });
+
     it('И ставит оценку', () => {
+        cy.getByTestId('ArticleDetails.Info');
+        cy.getByTestId('RatingCard').scrollIntoView();
+        cy.setRate(4, 'feedback');
+        cy.get('[data-selected=true]').should('have.length', 4);
+    });
+
+    it('И ставит оценку (с стабом на фикстурах)', () => {
+        cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
         cy.getByTestId('ArticleDetails.Info');
         cy.getByTestId('RatingCard').scrollIntoView();
         cy.setRate(4, 'feedback');
--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -1,9 +1,7 @@
 import { HTMLAttributeAnchorTarget, memo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { List, ListRowProps, WindowScroller } from 'react-virtualized';
 import { Text, TextSize } from '@/shared/ui/Text';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { PAGE_ID } from '@/widgets/Page';
 import { Article, ArticleView } from '../../model/types/article';
 import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
 import { ArticleListItemSkeleton } from '../ArticleListItem/ArticleListItemSkeleton';
@@ -15,12 +13,11 @@ interface ArticleListProps {
     isLoading?: boolean;
     view?: ArticleView;
     target?: HTMLAttributeAnchorTarget;
-    virtualized?: boolean;
 }
 
 const getSkeletons = (view: ArticleView) => new Array(view === ArticleView.BRIEF ? 9 : 3)
     .fill(0)
-    .map((item, idx) => (
+    .map((_, idx) => (
         <ArticleListItemSkeleton view={view} key={idx} />
     ));
 
@@ -33,42 +30,7 @@ export const ArticleList = memo((props: ArticleListProps) => {
         isLoading,
         view = ArticleView.BRIEF,
         target = '_blank',
-        virtualized = true,
     } = props;
-
-    const isBig = view === ArticleView.FULL;
-    const itemsPerRow = isBig ? 1 : 3;
-    const rowCount = isBig ? articles.length : Math.ceil(articles.length / itemsPerRow);
-
-    const rowRenderer = ({
-        index, key, style,
-    }: ListRowProps) => {
-        const items = [];
-        const fromIndex = index * itemsPerRow;
-        const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
-
-        for (let i = fromIndex; i < toIndex; i += 1) {
-            items.push(
-                <ArticleListItem
-                    article={articles[index]}
-                    view={view}
-                    target={target}
-                    key={`idx${i}`}
-                    className={cls.card}
-                />,
-            );
-        }
-
-        return (
-            <div
-                key={key}
-                style={style}
-                className={cls.row}
-            >
-                {items}
-            </div>
-        );
-    };
 
     if (!isLoading && !articles.length) {
         return (
@@ -79,50 +41,23 @@ export const ArticleList = memo((props: ArticleListProps) => {
     }
 
     return (
-        <WindowScroller
-            scrollElement={document.getElementById(`${PAGE_ID}`) as Element}
+        <div
+            data-testid="ArticleList"
+            className={classNames(cls.ArticleList, {}, [className, cls[view]])}
         >
-            {({
-                height,
-                width,
-                registerChild,
-                scrollTop,
-                onChildScroll,
-            }) => (
-                <div
-                    data-testid="ArticleList"
-                    ref={registerChild}
-                    className={classNames(cls.ArticleList, {}, [className, cls[view]])}
-                >
-                    {
-                        virtualized
-                            ? (
-                                <List
-                                    height={height ?? 700}
-                                    rowCount={rowCount}
-                                    rowHeight={isBig ? 700 : 300}
-                                    rowRenderer={rowRenderer}
-                                    width={width ? width - 80 : 700}
-                                    onScroll={onChildScroll}
-                                    scrollTop={scrollTop}
-                                    autoHeight
-                                />
-                            )
-                            : (
-                                articles.map((item) => (
-                                    <ArticleListItem
-                                        article={item}
-                                        view={view}
-                                        target={target}
-                                        key={item.id}
-                                        className={cls.card}
-                                    />
-                                ))
-                            )
-                    }
-                    {isLoading && getSkeletons(view)}
-                </div>
-            )}
-        </WindowScroller>
+            {
+                articles.map((item) => (
+                    <ArticleListItem
+                        article={item}
+                        view={view}
+                        target={target}
+                        key={item.id}
+                        className={cls.card}
+                    />
+                ))
+
+            }
+            {isLoading && getSkeletons(view)}
+        </div>
     );
 });
